ada6f5fa7d7e0d66c323fa1507a8c76547956f37
zoey <echohyt@sohu.com>
2018-02-08 00:01:45 +0800

    按照MVC简单封装了一下

--- resume4/js/sticky.js
+++ resume4/js/sticky.js
@@ -1,28 +1,11 @@
-findClosest()
-window.onscroll = function(x){
-    if(window.scrollY > 0){
-        topNavBar.classList.add('sticky')
-    }else{
-        topNavBar.classList.remove('sticky')
-    }
-    findClosest()
-}
-function findClosest(){
-    let specialTags = document.querySelectorAll('[data-x]')
-    let minIndex = 0
-    for(let i =1;i<specialTags.length; i++){
-        if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
-        minIndex = i
+!function(){
+    var view = document.querySelector('#topNavBar')
+    window.addEventListener('scroll',function(x){
+        if(window.scrollY > 0){
+            view.classList.add('sticky')
+        }else{
+            view.classList.remove('sticky')
         }
-    }
-    // minIndex 就是里窗口顶部最近的元素
-    specialTags[minIndex].classList.remove('offset')
-    let id = specialTags[minIndex].id
-    let a = document.querySelector('a[href="#'+ id + '"]')
-    let li = a.parentNode
-    let brothersAndMe = li.parentNode.children
-    for(let i=0; i<brothersAndMe.length; i++){
-        brothersAndMe[i].classList.remove('highlight')
-    }
-    li.classList.add('highlight')
-}
+    })
+}.call()
+
--- resume4/js/swiper.js
+++ resume4/js/swiper.js
@@ -1,14 +1,17 @@
-var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    
-    // 如果需要分页器
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    
-    // 如果需要前进后退按钮
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
+!function(){
+    var view = document.querySelector('#mySlides')
+    var mySwiper = new Swiper (view.querySelector('.swiper-container'), {
+      loop: true,
+      
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
+}.call()
